3bac507ecd44b60ed32bab380e82336f78655061
Ayman Chowdhury <156225408+ado1d@users.noreply.github.com>
2024-01-20 17:27:27 +0600

    added two methods to count number of set bits of a number

--- count_set_bits.cpp
+++ count_set_bits.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+int cnt_bits(int &n) {
+    int ans = 0;
+    while (n > 0) {
+        ans += n & 1;
+        n = n >> 1;
+    }
+    return ans;
+}
 
+//faster method to count set bits
+int cnt_bits_hack(int &n) {
+    int ans = 0;
+    while (n > 0) {
+        n = n & (n - 1);    //removes last set bits from current number
+        ans++;
+    }
+    return ans;
+}
+
+int main() {
+    int n;          cin >> n;
+    cout << cnt_bits_hack(n);
+}
